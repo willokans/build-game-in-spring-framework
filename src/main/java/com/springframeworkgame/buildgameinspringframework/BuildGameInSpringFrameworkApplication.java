5dc92e3a256500e0c4c4df74b96122f81ea70ae1
willokans <Slide2014!>
2022-11-09 14:07:56 +0000

    first commit

---
+++ src/main/java/com/springframeworkgame/buildgameinspringframework/BuildGameInSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.springframeworkgame.buildgameinspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildGameInSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildGameInSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframeworkgame/buildgameinspringframework/BuildGameInSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframeworkgame.buildgameinspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildGameInSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
